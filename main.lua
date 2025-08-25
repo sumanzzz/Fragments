8bb6170a08c9e1ffee5f6ad2bfb9fdac6d813391
Reegan <reegansuman@gmail.com>
2025-08-25 21:23:41 +0530

    font and FPS setup

--- main.lua
+++ main.lua
@@ -1,16 +1,29 @@
 local love = require "love"
-
+local fonts = {
+    small = {
+        font = love.graphics.getFont(16),
+        size = 16
+    },
+    medium = {
+        font = love.graphics.getFont(24),
+        size = 24
+    },
+    large = {
+        font = love.graphics.getFont(60),
+        size = 60
+    }
+}
 
 function love.load()
     
 end
 
 
-function love.update()
+function love.update(dt)
     
 end
 
 
-function love.update()
-    
+function love.draw()
+    love.graphics.printf("FPS :" ..love.timer.getFPS(),fonts.medium.font,10,love.graphics.getHeight()-30,love.graphics.getWidth())
 end
